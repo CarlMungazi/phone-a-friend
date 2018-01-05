3d4ce68b0c5eb27d62005bfc866a264a8f7752a2
Carl-Dev <c_mungazi@hotmail.co.uk>
2018-01-05 02:24:59 +0000

    Added SVG to search results view

--- views/contact-list.js
+++ views/contact-list.js
@@ -9,17 +9,17 @@ module.exports = {
         // let's create an avatar since we do not have initials
         let initial = `${contact.firstName} ${contact.lastName}`.split(' ').map((n) => n.substr(0,1)).slice(0,2).join('')
         return m('li.flex.items-center.lh-copy.pa3.ph0-l.bb.b--black-10',
-            m("svg[xmlns='http://www.w3.org/2000/svg'][xmlns:xlink='http://www.w3.org/1999/xlink'][viewBox='0 0 70 65'][width='50'][height='50']",
-              m("circle[fill='#ccc'][r='32'][cx='32'][cy='32']"),
-              m("text[x='32'][y='32'][text-anchor='middle'][stroke='#000'][dy='.3em']", initial),
+          m("svg[xmlns='http://www.w3.org/2000/svg'][xmlns:xlink='http://www.w3.org/1999/xlink'][viewBox='0 0 70 65'][width='50'][height='50']",
+            m("circle[fill='#ccc'][r='32'][cx='32'][cy='32']"),
+            m("text[x='32'][y='32'][text-anchor='middle'][stroke='#000'][dy='.3em']", initial),
           ),
           m('a', {
               href: `/edit/${contact.id}`, 
               oncreate: m.route.link
             },
             m('.pl3.flex-auto',
-              m('span.db.black-70', `${contact.firstName} ${contact.lastName}`),
-              m('span.db.black-70', `${contact.lastName}`)
+              m('span.f6.db.black-70', `${contact.firstName} ${contact.lastName}`),
+              m('span.f6.db.black-70', `${contact.lastName}`)
             )
           )
         )
