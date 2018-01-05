6949ef31f3f5070535976478027040e116d21b8a
Carl-Dev <c_mungazi@hotmail.co.uk>
2018-01-05 03:01:02 +0000

    Added edit functionality to single contact view

--- views/contact-edit.js
+++ views/contact-edit.js
@@ -4,29 +4,50 @@ const Contact = require('../model/contacts')
 module.exports = {
   oninit: function (vnode) {
     Contact.load(vnode.attrs.id)
+    vnode.state.inputDisabled = true
   },
-  view: function () {
+  view: function (vnode) {
     return m("form.pl0.mt0.measure.center", {
       onsubmit: function (e) {
         e.preventDefault()
         Contact.save()
       }
     }, [
-      m("label.f6.b.db.mb2", "First Name"),
-      m("input.input[type=text][placeholder=First Name].input-reset.bbo.b--black-20.pa2.mb2.dib.w-75.mr1", {
+      m("label.f7.b.db.mb2", "First Name"),
+      m("input.input[type=text][placeholder=First Name].f4.input-reset.bbo.b--black-20.pa2.mb2.dib.w-75.mr1", {
         oninput: m.withAttr("value", (value) => {
           Contact.current.firstName = value
         }),
-        value: Contact.current.firstName
+        value: Contact.current.firstName,
+        disabled: vnode.state.inputDisabled,
+        class: vnode.state.inputDisabled ? 'b--black-20' : 'b--light-blue'
       }),
-      m("label.f6.b.db.mb2", "Last Name"),
-      m("input.input[type=text][placeholder=Last Name].input-reset.bbo.b--black-20.pa2.mb2.dib.w-75.mr1", {
+      m("label.f7.b.db.mb2", "Last Name"),
+      m("input.input[type=text][placeholder=Last Name].f4.input-reset.bbo.b--black-20.pa2.mb2.dib.w-75.mr1", {
         oninput: m.withAttr("value", (value) => {
           Contact.current.lastName = value
         }),
-        value: Contact.current.lastName
+        value: Contact.current.lastName,
+        disabled: vnode.state.inputDisabled,
+        class: vnode.state.inputDisabled ? 'b--black-20' : 'b--light-blue'
       }),
-      m("button[type=submit].f6.dim.br2.ba.ph3.pv2.mb2.db.mid-gray.mt3", "Save"),
+      m('div',
+        m("button[type=button].f6.dim.br2.ba.ph3.pv2.mb2.dib.mid-gray.mt3.mr2", {
+          onclick: function (e) {
+            e.preventDefault()
+            vnode.state.inputDisabled = !vnode.state.inputDisabled
+          },
+          style: {
+            'pointer-events': vnode.state.inputDisabled ? 'initial' : 'none'
+          }
+        }, "Edit"),
+        m("button[type=submit].f6.dim.br2.ba.ph3.pv2.mb2.dib.mid-gray.mt3", {
+          onclick: function (e) {
+            e.preventDefault()
+            vnode.state.inputDisabled = !vnode.state.inputDisabled
+          }
+        }, "Save"),
+      )
     ])
   }
 }
