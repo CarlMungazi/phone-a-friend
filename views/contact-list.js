c44f6a599005dd002141ebfe33245d8ac83ad756
Carl-Dev <c_mungazi@hotmail.co.uk>
2018-01-05 02:17:03 +0000

    Added SVG avatar to contact list view

--- views/contact-list.js
+++ views/contact-list.js
@@ -3,16 +3,22 @@ const Contact = require('../model/contacts')
 
 module.exports = {
   oninit: Contact.loadList,
-  view: function() {
+  view: function(vnode) {
     return m('ul.list.pl0.mt0.measure.center',
       Contact.list.map(contact => {
+        // let's create an avatar since we do not have initials
+        let initial = `${contact.firstName} ${contact.lastName}`.split(' ').map((n) => n.substr(0,1)).slice(0,2).join('')
         return m('li.flex.items-center.lh-copy.pa3.ph0-l.bb.b--black-10',
+            m("svg[xmlns='http://www.w3.org/2000/svg'][xmlns:xlink='http://www.w3.org/1999/xlink'][viewBox='0 0 70 65'][width='50'][height='50']",
+              m("circle[fill='#ccc'][r='32'][cx='32'][cy='32']"),
+              m("text[x='32'][y='32'][text-anchor='middle'][stroke='#000'][dy='.3em']", initial),
+          ),
           m('a', {
               href: `/edit/${contact.id}`, 
               oncreate: m.route.link
             },
-            m('div.pl3.flex-auto',
-              m('span.db.black-70', `${contact.firstName}`),
+            m('.pl3.flex-auto',
+              m('span.db.black-70', `${contact.firstName} ${contact.lastName}`),
               m('span.db.black-70', `${contact.lastName}`)
             )
           )
@@ -21,3 +27,4 @@ module.exports = {
     )
   }
 }
+
